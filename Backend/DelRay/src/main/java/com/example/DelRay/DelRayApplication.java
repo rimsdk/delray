99492eeec80f8855ec86d59785780961da064a4b
Sadki Rim <128902889+rimsdk@users.noreply.github.com>
2024-11-16 00:58:15 +0100

    initialisation

---
+++ Backend/DelRay/src/main/java/com/example/DelRay/DelRayApplication.java
@@ -0,0 +1,13 @@
+package com.example.DelRay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelRayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelRayApplication.class, args);
+	}
+
+}
---
+++ Backend/DelRay/src/test/java/com/example/DelRay/DelRayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DelRay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelRayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
